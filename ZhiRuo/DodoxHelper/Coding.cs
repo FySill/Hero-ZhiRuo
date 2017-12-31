9d8086a818f049e0a8f1bc7acf548c2f497bb160
Xujun Ren <v-xujren@microsoft.com>
2017-12-31 09:21:50 +0800

    change code structure

---
+++ ZhiRuo/DodoxHelper/Coding.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DodoxHelper
+{
+    public class Coding
+    {
+        public static void ChangeCoding2UTF8()
+        {
+            string dir = @"C:\Users\v-xujren\source\repos\ZhiRuo\ZhiRuo\ZhiRuo\HeroII\Maps";
+
+            List<string> destfiles = new List<string>();
+         
+            foreach (var eachfileinfo in new DirectoryInfo(dir).GetFiles())
+            {
+                destfiles.Add(eachfileinfo.FullName);
+            }
+
+
+            foreach (string destfile in destfiles)
+            {
+                File.WriteAllText(destfile, File.ReadAllText(destfile, Encoding.Default), Encoding.UTF8);
+            }
+            Console.WriteLine("finished.");
+
+        }
+    }
+}
---
+++ ZhiRuo/DodoxHelper/GenerateDoDoxCode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DodoxHelper
+{
+    public class GenerateDoDoxCode
+    {
+        public static string   Generate()
+        {
+            string str = string.Empty;
+            int j = 1;
+
+            for (int i = 82; i < 96; i++, j++)
+            {
+
+                str += $"$gameVariables.setValue({i},$gameVariables.value({i}) + 99);";
+            }
+            return str;
+        }
+    }
+}
